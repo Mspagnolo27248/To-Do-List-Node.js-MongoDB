c6aa833efe3f26838234d825787a94dda971b011
Michael Spagnolo <Mspagnolo27@gmail.com>
2021-11-05 13:06:13 -0400

    array -v delete works

--- app.js
+++ app.js
@@ -18,18 +18,30 @@ const topics = ["Go to Sleep","Pick Nose"]
 let today = new Date()
 
 //Routes
+//#Get Home
 app.get("/",(req,res)=>{
     res.render("index",{topics:topics,currentDay:today})
 })
 
-
+//#POST Home
 app.post("/",function(req,res){
  var item = req.body.newItem
+ 
  topics.push(item)
 res.render("index.ejs",{topics:topics,currentDay:today})
 });
 
-
+//#Post Home/Delete
+app.post("/delete",function(req,res){
+    var selectedItem = req.body.itemBox;
+    var itemIndex = topics.indexOf(selectedItem);
+    console.log(selectedItem);
+    console.log(itemIndex);
+    if  (itemIndex>-1){
+        topics.splice(itemIndex,1);
+    }
+    res.redirect("/")
+});
 
 //Run App
 app.listen(3000,function(){
